4e4a4ec510ebb3eebf766401d86287cfa15e6b40
Maizels Kirill <domo.ddr@gmail.com>
2018-05-14 21:03:44 +0600

    Homework done

--- TasksApp/MainWindow.xaml.cs
+++ TasksApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,75 @@ using System.Windows.Shapes;
 
 namespace TasksApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource cancellationTokenSource;
+        private CancellationToken cancellationToken;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Начало работы
+        private void StartTaskBtnClick(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
+
+            // Отключение кнопки 'Начать работу'
+            StartTaskBtn.IsEnabled = false;
+
+            Task newTask = new Task(new Action(DoWork), cancellationToken);
+            newTask.Start();
+        }
+
+        // Имитация работы
+        public void DoWork()
+        {
+            Random random = new Random();
+
+            int workTime = random.Next(10000, 20000);
+
+            // Выставление максимального значения Progress Bar
+            Dispatcher.Invoke(() =>
+            {
+                TaskProgress.Maximum = workTime;
+            });
+
+            // Прогресс работы через каждые 0.2 секунды
+            for (int i = 0; i < workTime; i += 200)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return;
+
+                Dispatcher.Invoke(() =>
+                {
+                    TaskProgress.Value += 200;
+                });
+
+                Thread.Sleep(200);
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+                ClearProgressBar();
+            });
+        }
+
+        // Отмена работы
+        private void CancelBtnClick(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+
+            ClearProgressBar();
+        }
+
+        // Обнуление Progress Bar
+        private void ClearProgressBar()
+        {
+            StartTaskBtn.IsEnabled = true;
+            TaskProgress.Value = 0;
         }
     }
 }
